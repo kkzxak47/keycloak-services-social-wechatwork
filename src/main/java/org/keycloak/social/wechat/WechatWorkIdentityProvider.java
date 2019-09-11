409d0f6673101f01ee4df61112056770c891e6bb
钟逊 <zhongxun@xsyxsc.com>
2019-09-11 13:07:12 +0800

    add user attributes

--- src/main/java/org/keycloak/social/wechat/WechatWorkIdentityProvider.java
+++ src/main/java/org/keycloak/social/wechat/WechatWorkIdentityProvider.java
@@ -23,6 +23,7 @@ import org.infinispan.manager.DefaultCacheManager;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.broker.oidc.AbstractOAuth2IdentityProvider;
 import org.keycloak.broker.oidc.mappers.AbstractJsonUserAttributeMapper;
+
 import org.keycloak.broker.provider.AuthenticationRequest;
 import org.keycloak.broker.provider.BrokeredIdentityContext;
 import org.keycloak.broker.provider.IdentityBrokerException;
@@ -34,6 +35,7 @@ import org.keycloak.events.EventBuilder;
 import org.keycloak.events.EventType;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
 import org.keycloak.services.ErrorPage;
 import org.keycloak.services.messages.Messages;
 
@@ -66,6 +68,10 @@ public class WechatWorkIdentityProvider extends AbstractOAuth2IdentityProvider<W
 
     public static final String WEIXIN_CORP_ID = "corpid";
     public static final String WEIXIN_CORP_SECRET = "corpsecret";
+    public static final String PROFILE_MOBILE = "mobile";
+    public static final String PROFILE_GENDER = "gender";
+    public static final String PROFILE_STATUS = "status";
+    public static final String PROFILE_ENABLE = "enable";
 
     private String ACCESS_TOKEN_KEY = "access_token";
     private String ACCESS_TOKEN_CACHE_KEY = "wechat_work_sso_access_token";
@@ -105,8 +111,7 @@ public class WechatWorkIdentityProvider extends AbstractOAuth2IdentityProvider<W
                     if (j == null) {
                         throw new Exception("renew access token error");
                     }
-                    // TODO remove in production, may leak access token to log
-//                    logger.info("retry in renew access token " + j.toString());
+                    logger.debug("retry in renew access token " + j.toString());
                 }
                 token = getJsonProperty(j, ACCESS_TOKEN_KEY);
                 long timeout = Integer.valueOf(getJsonProperty(j, "expires_in"));
@@ -158,29 +163,31 @@ public class WechatWorkIdentityProvider extends AbstractOAuth2IdentityProvider<W
 
     @Override
     protected BrokeredIdentityContext extractIdentityFromProfile(EventBuilder event, JsonNode profile) {
+        logger.info(profile.toString());
         // profile: see https://work.weixin.qq.com/api/doc#90000/90135/90196
-        BrokeredIdentityContext user = new BrokeredIdentityContext(
+        BrokeredIdentityContext identity = new BrokeredIdentityContext(
                 (getJsonProperty(profile, "userid")));
 
-        user.setUsername(getJsonProperty(profile, "userid").toLowerCase());
-        user.setBrokerUserId(getJsonProperty(profile, "userid").toLowerCase());
-        user.setModelUsername(getJsonProperty(profile, "userid").toLowerCase());
-        user.setFirstName(getJsonProperty(profile, "email").split("@")[0].toLowerCase());
-        user.setLastName(getJsonProperty(profile, "name"));
-        user.setEmail(getJsonProperty(profile, "email").toLowerCase());
+        identity.setUsername(getJsonProperty(profile, "userid").toLowerCase());
+        identity.setBrokerUserId(getJsonProperty(profile, "userid").toLowerCase());
+        identity.setModelUsername(getJsonProperty(profile, "userid").toLowerCase());
+        identity.setFirstName(getJsonProperty(profile, "email").split("@")[0].toLowerCase());
+        identity.setLastName(getJsonProperty(profile, "name"));
+        identity.setEmail(getJsonProperty(profile, "email").toLowerCase());
         // 手机号码，第三方仅通讯录应用可获取
-        user.setUserAttribute("mobile", getJsonProperty(profile, "mobile"));
+        identity.setUserAttribute(PROFILE_MOBILE, getJsonProperty(profile, "mobile"));
         // 性别。0表示未定义，1表示男性，2表示女性
-        user.setUserAttribute("gender", getJsonProperty(profile, "gender"));
+        identity.setUserAttribute(PROFILE_GENDER, getJsonProperty(profile, "gender"));
         // 激活状态: 1=已激活，2=已禁用，4=未激活。
         // 已激活代表已激活企业微信或已关注微工作台（原企业号）。未激活代表既未激活企业微信又未关注微工作台（原企业号）。
-        user.setUserAttribute("status", getJsonProperty(profile, "status"));
+        identity.setUserAttribute(PROFILE_STATUS, getJsonProperty(profile, "status"));
         // 成员启用状态。1表示启用的成员，0表示被禁用。注意，服务商调用接口不会返回此字段
-        user.setUserAttribute("enable", getJsonProperty(profile, "enable"));
-        user.setIdpConfig(getConfig());
-        user.setIdp(this);
-        AbstractJsonUserAttributeMapper.storeUserProfileForMapper(user, profile, getConfig().getAlias());
-        return user;
+        identity.setUserAttribute(PROFILE_ENABLE, getJsonProperty(profile, "enable"));
+
+        identity.setIdpConfig(getConfig());
+        identity.setIdp(this);
+        AbstractJsonUserAttributeMapper.storeUserProfileForMapper(identity, profile, getConfig().getAlias());
+        return identity;
     }
 
     public BrokeredIdentityContext getFederatedIdentity(String authorizationCode) {
@@ -330,5 +337,13 @@ public class WechatWorkIdentityProvider extends AbstractOAuth2IdentityProvider<W
             return ErrorPage.error(session, null, Response.Status.BAD_GATEWAY,
                     Messages.IDENTITY_PROVIDER_UNEXPECTED_ERROR);
         }
+    }
+
+    @Override
+    public void updateBrokeredUser(KeycloakSession session, RealmModel realm, UserModel user, BrokeredIdentityContext context) {
+        user.setSingleAttribute(PROFILE_MOBILE, context.getUserAttribute(PROFILE_MOBILE));
+        user.setSingleAttribute(PROFILE_GENDER, context.getUserAttribute(PROFILE_GENDER));
+        user.setSingleAttribute(PROFILE_STATUS, context.getUserAttribute(PROFILE_STATUS));
+        user.setSingleAttribute(PROFILE_ENABLE, context.getUserAttribute(PROFILE_ENABLE));
     }
 }
